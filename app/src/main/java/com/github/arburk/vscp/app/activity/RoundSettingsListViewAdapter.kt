a50700d5e7f16c617696c0620c0c70d89f960542
Arno Burkhart <arburk@users.noreply.github.com>
2023-06-04 13:01:32 +0200

    #1 defined basis layout for blind round configuration

--- app/src/main/java/com/github/arburk/vscp/app/activity/RoundSettingsListViewAdapter.kt
+++ app/src/main/java/com/github/arburk/vscp/app/activity/RoundSettingsListViewAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.EditText
 import android.widget.TextView
 import com.github.arburk.vscp.app.R
+import kotlin.math.ceil
 
 class RoundSettingsListViewAdapter(
 
@@ -33,14 +35,22 @@ class RoundSettingsListViewAdapter(
     }
 
     linearLayoutView!!.findViewById<TextView>(R.id.round_id).text =
-      context.getString(R.string.round_id, position + 1)
+      context.getString(R.string.round_id, formattedNumberOfCurrentRound(position))
 
     val currentBlind = getItem(position).rounds.value!!
-    linearLayoutView.findViewById<TextView>(R.id.small_big).text =
-      context.getString(R.string.small_big_blind_values, currentBlind.small, currentBlind.big)
+    linearLayoutView.findViewById<EditText>(R.id.small_blind).setText(currentBlind.small.toString())
+    linearLayoutView.findViewById<TextView>(R.id.big_blind).text = currentBlind.big.toString()
 
     return linearLayoutView
   }
+
+  private fun formattedNumberOfCurrentRound(position: Int) =
+    (position + 1).toString().padStart(calculatePadLength(), '0')
+
+  /**
+   * dynamically get the number of leading zeros based on configured blind rounds
+   */
+  private fun calculatePadLength() = ceil(count.toDouble() / 10).toInt() + 1
 
 
 }
