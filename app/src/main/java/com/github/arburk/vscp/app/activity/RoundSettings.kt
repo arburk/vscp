3f408cbe4e6bdd7c9f76456309bada680f023d21
Arno Burkhart <arburk@users.noreply.github.com>
2023-06-04 14:09:36 +0200

    #1 Implemented adding / removing blind rounds

--- app/src/main/java/com/github/arburk/vscp/app/activity/RoundSettings.kt
+++ app/src/main/java/com/github/arburk/vscp/app/activity/RoundSettings.kt
@@ -4,20 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.github.arburk.vscp.app.MainActivity
 import com.github.arburk.vscp.app.R
 import com.github.arburk.vscp.app.databinding.FragmentRoundSettingsBinding
+import com.github.arburk.vscp.app.model.Blind
 import com.github.arburk.vscp.app.service.TimerService
-import java.util.*
 
 class RoundSettings : Fragment() {
 
   private var _binding: FragmentRoundSettingsBinding? = null
   private val binding get() = _binding!!
   private lateinit var timerService: TimerService
-  private var arrayList: List<RoundSettingsViewModel> = Collections.emptyList()
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
     _binding = FragmentRoundSettingsBinding.inflate(inflater, container, false)
@@ -26,13 +26,36 @@ class RoundSettings : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     timerService = (activity as MainActivity).getTimerService()
-    arrayList = timerService.getRounds().map { elem -> RoundSettingsViewModel().apply { initData(elem) } }
     initLayout()
   }
 
   private fun initLayout() {
+    val data = timerService.getRounds().map { elem -> RoundSettingsViewModel().apply { initData(elem) } }
     val listViewOfRows = activity?.findViewById<ListView>(R.id.rounds_row_list_view)
-    listViewOfRows?.adapter = RoundSettingsListViewAdapter(requireContext(), arrayList)
+    listViewOfRows?.adapter = RoundSettingsListViewAdapter(requireContext(), data)
+
+    activity?.findViewById<ImageButton>(R.id.add_blind_button)?.setOnClickListener { addBlind() }
+    activity?.findViewById<ImageButton>(R.id.remove_last_blind_button)?.setOnClickListener { removeBlind() }
   }
 
+  private fun addBlind() {
+    var newRound = Blind(1, 2)
+    timerService.getRounds().also {
+      if (it.isNotEmpty()) {
+        val lastRound = it[it.size - 1]
+        newRound = Blind(lastRound.big, lastRound.big * 2)
+      }
+      timerService.setRounds(it.plus(newRound))
+    }
+    initLayout()
+  }
+
+  private fun removeBlind() {
+    timerService.getRounds().also {
+      if (it.isNotEmpty()) {
+        timerService.setRounds(it.dropLast(1).toTypedArray())
+        initLayout()
+      }
+    }
+  }
 }
--- app/src/main/java/com/github/arburk/vscp/app/activity/RoundSettingsListViewAdapter.kt
+++ app/src/main/java/com/github/arburk/vscp/app/activity/RoundSettingsListViewAdapter.kt
@@ -1,13 +1,19 @@
 package com.github.arburk.vscp.app.activity
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import com.github.arburk.vscp.app.R
+import com.github.arburk.vscp.app.model.Blind
 import kotlin.math.ceil
 
 class RoundSettingsListViewAdapter(
@@ -38,10 +44,26 @@ class RoundSettingsListViewAdapter(
       context.getString(R.string.round_id, formattedNumberOfCurrentRound(position))
 
     val currentBlind = getItem(position).rounds.value!!
-    linearLayoutView.findViewById<EditText>(R.id.small_blind).setText(currentBlind.small.toString())
+    val smallBlindEditText = linearLayoutView.findViewById<EditText>(R.id.small_blind)
+    smallBlindEditText.addTextChangedListener { BlindTextWatcher(currentBlind) }
+    smallBlindEditText.setText(currentBlind.small.toString())
+
     linearLayoutView.findViewById<TextView>(R.id.big_blind).text = currentBlind.big.toString()
 
+    linearLayoutView.findViewById<ImageButton>(R.id.derease_button).setOnClickListener { decreaseBlind(currentBlind) }
+    linearLayoutView.findViewById<ImageButton>(R.id.inrease_button).setOnClickListener { increaseBlind(currentBlind) }
+
     return linearLayoutView
+  }
+
+  private fun increaseBlind(currentBlind: Blind) {
+    // TODO("Not yet implemented")
+    Log.v("RoundSettingsListViewAdapter", "TODO: increaseBlind for $currentBlind")
+  }
+
+  private fun decreaseBlind(currentBlind: Blind) {
+    // TODO("Not yet implemented")
+    Log.v("RoundSettingsListViewAdapter", "TODO: decreaseBlind for $currentBlind")
   }
 
   private fun formattedNumberOfCurrentRound(position: Int) =
@@ -53,4 +75,24 @@ class RoundSettingsListViewAdapter(
   private fun calculatePadLength() = ceil(count.toDouble() / 10).toInt() + 1
 
 
+  inner class BlindTextWatcher(currentBlind: Blind) : TextWatcher {
+
+    private var blind: Blind
+
+    init {
+      blind = currentBlind
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+      Log.v("RoundSettingsListViewAdapter", "TODO: beforeTextChanged $blind")
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+      Log.v("RoundSettingsListViewAdapter", "TODO: onTextChanged $blind")
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+      Log.v("RoundSettingsListViewAdapter", "TODO: afterTextChanged $blind")
+    }
+  }
 }
