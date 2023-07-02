a0c1411d4683f20c6317fde2220f0f7b173840be
Arno Burkhart <arburk@users.noreply.github.com>
2023-07-02 15:56:57 +0200

    Renamed WelcomeScreen to MainScreen

--- app/src/main/java/com/github/arburk/vscp/app/activity/MainScreen.kt
+++ app/src/main/java/com/github/arburk/vscp/app/activity/MainScreen.kt
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.github.arburk.vscp.app.R
-import com.github.arburk.vscp.app.databinding.WelcomeScreenBinding
+import com.github.arburk.vscp.app.databinding.MainScreenBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class WelcomeScreen : Fragment() {
+class MainScreen : Fragment() {
 
-  private var _binding: WelcomeScreenBinding? = null
+  private var _binding: MainScreenBinding? = null
 
   // This property is only valid between onCreateView and
   // onDestroyView.
@@ -25,7 +25,7 @@ class WelcomeScreen : Fragment() {
     savedInstanceState: Bundle?
   ): View {
 
-    _binding = WelcomeScreenBinding.inflate(inflater, container, false)
+    _binding = MainScreenBinding.inflate(inflater, container, false)
     return binding.root
 
   }
@@ -34,7 +34,7 @@ class WelcomeScreen : Fragment() {
     super.onViewCreated(view, savedInstanceState)
 
     binding.pokerTimer.setOnClickListener {
-      findNavController().navigate(R.id.action_WelcomeScreen_to_Timer)
+      findNavController().navigate(R.id.action_MainScreen_to_Timer)
     }
   }
 
