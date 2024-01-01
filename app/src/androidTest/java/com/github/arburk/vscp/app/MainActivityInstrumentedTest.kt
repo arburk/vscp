21979ffc1853f7735129d08e0581a137a2ea3a32
Arno Burkhart <arburk@users.noreply.github.com>
2024-01-01 19:27:39 +0100

    #9: adding first example of instrumented test

--- app/src/androidTest/java/com/github/arburk/vscp/app/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/arburk/vscp/app/MainActivityInstrumentedTest.kt
@@ -1,23 +1,44 @@
 package com.github.arburk.vscp.app
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import junit.framework.TestCase.assertEquals
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers
+import de.mannodermaus.junit5.ActivityScenarioExtension
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.RegisterExtension
 
-import org.junit.Test
-import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+
 class MainActivityInstrumentedTest {
+
+  @JvmField
+  @RegisterExtension
+  val scenarioExtension = ActivityScenarioExtension.launch<MainActivity>()
+
   @Test
-  fun useAppContext() {
+  fun navigateToTimer(scenario: ActivityScenario<MainActivity>) {
     // Context of the app under test.
-    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.github.arburk.vscp.app", appContext.packageName)
+    scenario.onActivity {
+      assertEquals("com.github.arburk.vscp.app", it.packageName)
+      assertEquals(1, it.supportFragmentManager.fragments.size)
+      val currentFragment = it.supportFragmentManager.fragments[0].childFragmentManager.fragments[0]
+      assertEquals("com.github.arburk.vscp.app.activity.MainScreen", currentFragment.javaClass.name)
+    }
+
+    onView(ViewMatchers.withId(R.id.poker_timer)).perform(click())
+
+    scenario.onActivity {
+      assertEquals(1, it.supportFragmentManager.fragments.size)
+      val currentFragment = it.supportFragmentManager.fragments[0].childFragmentManager.fragments[0]
+      assertEquals("com.github.arburk.vscp.app.activity.PokerTimer", currentFragment.javaClass.name)
+    }
   }
+
 }
